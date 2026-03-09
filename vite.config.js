67141f6d5d46dad8f7f23eb4a35f2ca9a673e7bb
Alexander <alexander.marizan@mia-technologies.com>
2026-03-09 09:42:12 -0400

    Configurar GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/gestor-peliculas-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
